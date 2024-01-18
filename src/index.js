9a91518800be0ba0395dc76d60fccd5e459d2320
louhoon <louane.voisin@hotmail.com>
2024-01-18 20:38:46 +0100

    added refresh weather to get actual temperature for cities

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
+function refreshWeather(response) {
+	let temperatureElement = document.querySelector("#current-temperature");
+	let temperature = response.data.temperature.current;
+	temperatureElement.innerHTML = `${Math.round(temperature)}°`;
+	let cityElement = document.querySelector("#city");
+	cityElement.innerHTML = response.data.city;
+}
+
+
+function searchCity(city) {
+  let apiKey = "3b7o30109b4aft0712c02563ba2bbf20";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function displayCurrentCity(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let search = document.querySelector("#search-form");
